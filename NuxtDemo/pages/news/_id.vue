2a18d6e407d5dd33a6e824e022e61fbd9bf5f3f1
zengfan <zengfan230@163.com>
2019-01-21 17:30:48 +0800

    nuxt学习

---
+++ NuxtDemo/pages/news/_id.vue
@@ -0,0 +1,40 @@
+
+<template>
+
+  <div>
+
+    <h2>news content</h2>
+
+
+    <p>NewsID:{{$route.params.id}}</p>
+
+
+    <ul>
+      <li><a href="/">Home</a></li>
+
+    </ul>
+  </div>
+</template>
+<script>
+
+  export default {
+    validate ({ params }) {
+      // Must be a number
+      return /^\d+$/.test(params.id)
+    },data(){
+      return{
+        title:this.$route.params.title,
+      }
+    },
+    //独立设置head信息
+    head(){
+      return{
+        title:this.title,
+        meta:[
+          {hid:'description',name:'news',content:'This is news page'}
+        ]
+      }
+    }
+  }
+
+</script>
